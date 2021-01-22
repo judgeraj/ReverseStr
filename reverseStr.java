0d62645e80ed553600a99e18e1de2bf51d5aaff2
judgeraj <judgeraj42@gmail.com>
2021-01-21 16:28:33 -0800

    WORKING RECURSIVE SOLUTION

---
+++ reverseStr.java
@@ -0,0 +1,16 @@
+public void reverseString(char[] s) {
+       reverse(0, s.length - 1, s);
+       return;
+   }
+    
+   public void reverse(int index, int last, char[] s) {
+       if(index > last) {
+           return;
+       }
+       //swap
+       char temp = s[index];
+       s[index] = s[last];
+       s[last] = temp;
+            
+        reverse(++index, --last, s);
+}
